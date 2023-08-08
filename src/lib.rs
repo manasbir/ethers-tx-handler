8010edc6b227aef57743bcf2c9f496e51d17755d
manas <bagrimanasbir@gmail.com>
2023-08-08 05:12:39 -0700

    removed uneccessaary wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 pub mod types;
 
-use std::sync::Arc;
-
 use ethers::{
     prelude::signer::SignerMiddlewareError,
     providers::{JsonRpcClient, Middleware, PendingTransaction},
@@ -11,60 +9,48 @@ use tracing::{error, info};
 
 use crate::types::{TxErrors, TxStatus};
 
-/// Wrapper for ethers client/SignerMiddleware
-pub struct ClientWrapper<M> {
-    pub client: Arc<M>,
-}
 
-impl<M> ClientWrapper<M>
-where
-    M: Middleware + 'static,
-{
-    pub fn new(client: Arc<M>) -> Self {
-        Self { client }
-    }
-    /// Handles the transaction and returns a TxStatus
-    pub async fn handle_tx<P: JsonRpcClient, S: Signer>(
-        &self,
-        tx: Result<PendingTransaction<'_, P>, SignerMiddlewareError<impl Middleware, S>>,
-    ) -> TxStatus {
-        let tx = tx.map_err(|e| format!("Failed to send transaction: {:?}", e));
+/// Handles the transaction and returns a TxStatus
+pub async fn handle_tx<P: JsonRpcClient, S: Signer>(
+    tx: Result<PendingTransaction<'_, P>, SignerMiddlewareError<impl Middleware, S>>,
+) -> TxStatus {
+    let tx = tx.map_err(|e| format!("Failed to send transaction: {:?}", e));
 
-        let tx = match tx {
-            Ok(tx) => tx,
-            Err(e) => {
-                error!("Failed to send transaction: {:?}", e);
-                return TxStatus::Failed(TxErrors::Failed(e));
+    let tx = match tx {
+        Ok(tx) => tx,
+        Err(e) => {
+            error!("Failed to send transaction: {:?}", e);
+            return TxStatus::Failed(TxErrors::Failed(e));
+        }
+    };
+
+    let hash = tx.tx_hash();
+
+    info!("Mining tx hash: {:?}", hash);
+    let receipt = tx
+        .await
+        .map_err(|e| format!("Failed to mine transaction: {:?}", e));
+    let receipt = match receipt {
+        Ok(receipt) => receipt,
+        Err(e) => return TxStatus::Failed(TxErrors::Failed(e)),
+    };
+    match receipt {
+        Some(receipt) => {
+            info!("Tx mined!");
+            if receipt.status == Some(0.into()) {
+                info!("Tx reverted!");
+                return types::TxStatus::Failed(TxErrors::Reverted(receipt));
             }
-        };
-
-        let hash = tx.tx_hash();
-
-        info!("Mining tx hash: {:?}", hash);
-        let receipt = tx
-            .await
-            .map_err(|e| format!("Failed to mine transaction: {:?}", e));
-        let receipt = match receipt {
-            Ok(receipt) => receipt,
-            Err(e) => return TxStatus::Failed(TxErrors::Failed(e)),
-        };
-        match receipt {
-            Some(receipt) => {
-                info!("Tx mined!");
-                if receipt.status == Some(0.into()) {
-                    info!("Tx reverted!");
-                    return types::TxStatus::Failed(TxErrors::Reverted(receipt));
-                }
-                TxStatus::Successful(receipt)
-            }
-            None => {
-                error!("No receipt for tx hash: {:?}", hash);
-                // I don't even think this is possible
-                TxStatus::Failed(TxErrors::NoReceipt(hash))
-            }
+            TxStatus::Successful(receipt)
+        }
+        None => {
+            error!("No receipt for tx hash: {:?}", hash);
+            // I don't even think this is possible
+            TxStatus::Failed(TxErrors::NoReceipt(hash))
         }
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -74,6 +60,8 @@ mod tests {
         types::TransactionRequest,
         utils::{parse_ether, Anvil},
     };
+
+    use crate::handle_tx;
 
     #[tokio::test]
     async fn it_works() {
@@ -89,16 +77,15 @@ mod tests {
 
         let tx = TransactionRequest::pay(anvil.addresses()[0], 100);
 
-        let wrapped_client = super::ClientWrapper::new(Arc::new(client.clone()));
 
         let tx = client.send_transaction(tx, None).await;
-        let status = wrapped_client.handle_tx(tx).await;
+        let status = handle_tx(tx).await;
         println!("{:?}", status);
 
         let tx_2 = TransactionRequest::pay(anvil.addresses()[1], parse_ether("10000000").unwrap());
         let tx = client.send_transaction(tx_2, None).await;
 
-        let status = wrapped_client.handle_tx(tx).await;
+        let status = handle_tx(tx).await;
         println!("{:?}", status);
     }
 }
