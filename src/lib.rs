5306a5e66353798cb396f51fbace5547c9357b36
manas <bagrimanasbir@gmail.com>
2023-08-08 02:52:37 -0700

    documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,18 @@ pub mod types;
 
 use std::sync::Arc;
 
-use ethers::{providers::{Middleware, PendingTransaction, JsonRpcClient}, prelude::{signer::SignerMiddlewareError, k256::ecdsa::SigningKey}, signers::Wallet, types::TransactionReceipt};
+use ethers::{providers::{Middleware, PendingTransaction, JsonRpcClient}, prelude::{signer::SignerMiddlewareError, k256::ecdsa::SigningKey}, signers::Wallet};
 
 use crate::types::{TxErrors, TxStatus};
 
-
+/// Wrapper for ethers client/SignerMiddleware
 pub struct ClientWrapper<M> {
     pub client: Arc<M>,
 }
 
 impl <M: Middleware + 'static + JsonRpcClient> ClientWrapper<M> {
-    async fn handle_tx(&self, tx: Result<PendingTransaction<'_, M>, SignerMiddlewareError<M, Wallet<SigningKey>>,>) -> TxStatus {
+    /// Handles the transaction and returns a TxStatus
+    pub async fn handle_tx(&self, tx: Result<PendingTransaction<'_, M>, SignerMiddlewareError<M, Wallet<SigningKey>>,>) -> TxStatus {
         let tx = tx.map_err(|e| format!("Failed to send transaction: {:?}", e));
 
         let tx = match tx {
--- src/types.rs
+++ src/types.rs
@@ -1,17 +1,25 @@
-use ethers::types::{Address, H256, U64, TransactionReceipt};
-use serde::{Deserialize, Serialize};
+use ethers::types::{H256, TransactionReceipt};
+use serde::Serialize;
 
 // minimal types since they are pretty case specific
 
+
+/// enum for success and failure
 #[derive(Debug, Serialize, Clone)]
 pub enum TxStatus {
+    /// Transaction was successful and executed
     Successful(TransactionReceipt),
+    /// Transaction failed in one way or another
     Failed(TxErrors),
 }
 
+/// enum for the 3 different types of failures
 #[derive(Debug, Serialize, Clone)]
 pub enum TxErrors {
+    /// Transaction went through, but reciept is None
     NoReceipt(H256),
+    /// Transaction reverted
     Reverted(TransactionReceipt),
+    /// Transaction failed to send or mine
     Failed(String),
 }
